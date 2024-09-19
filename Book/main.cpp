334ac5a16aaf23407c4db994d1ab9e6af1e25736
varchunn <varvaram186@gmail.com>
2024-09-19 21:38:36 +0300

    Initial Commit

---
+++ Book/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Book
+//
+//  Created by Варя Мінєнкова on 19.09.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
